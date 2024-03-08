211fdc7048e90aa3228ed2406d9c09f0332fba82
Lorenzo Albornoz <lorenzoa.28.la@gmail.com>
2024-03-08 08:17:26 -0300

    input

---
+++ inputs/input_numeros.py
@@ -0,0 +1,13 @@
+#pedirle un número al usuario
+
+numero= input("dame un numero, capitan:")
+
+#convierto el numero a entero y lo multiplico por 2
+# resultado_entero = int(numero) *2
+
+#convierto el numero a flotante y lo multiplico por 2
+resultado_flotante = float(numero) *2
+
+#mostrando resultado
+print(resultado_flotante)
+
