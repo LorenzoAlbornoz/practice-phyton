d574dc6af49d9c6c8614763b0501da0baf9128a2
Lorenzo Albornoz <lorenzoa.28.la@gmail.com>
2024-03-18 18:19:18 -0300

    modulos

---
+++ modulos/modulo_saludar.py
@@ -0,0 +1,8 @@
+def Saludar(name):
+    return f"¡Hola {name}! Espero que hayas tenido un maravilloso dìa"
+
+def Saludar_raro(name):
+    return f"¡Konda {name}! Espero que estes nashe, anasheli anasheiii"
+saludo = "jaja"
+
+print(__name__)
